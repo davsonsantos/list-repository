7a88a9eaec1182161cfc2cab0fb8ec4baf629f0c
Davson Santos <davsonsantos@gmail.com>
2022-07-04 11:24:15 -0300

    ajuste config

--- config.php
+++ config.php
@@ -1,11 +1,8 @@
 <?php
 
-define('TOKEN_GIT_USER', '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');
+define('TOKEN_GIT_USER', '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');
 define('GIT_USER', 'davsonsantos');
 
-//DEFINE O CALLBACK E RECUPERA O POST
-$jSON = null;
-$PostData = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
 use Github\Client;
 
@@ -14,6 +11,8 @@ $client = new Client();
 $client = new \Github\Client();
 $client->authenticate(TOKEN_GIT_USER, null, \Github\AuthMethod::ACCESS_TOKEN);
 
+//DEFINE O CALLBACK E RECUPERA O POST
+$jSON = null;
+$PostData = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
 $jSON['ajax_response'] = "";
-
-
--- github.php
+++ github.php
@@ -3,11 +3,4 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config.php';
 
-
-use Github\Client;
-
-$client = new Client();
-
-$client = new \Github\Client();
-$client->authenticate(TOKEN_GIT_USER, null, \Github\AuthMethod::ACCESS_TOKEN);
 $repositories = $client->currentUser()->repositories(GIT_USER,'full_name','asc');
